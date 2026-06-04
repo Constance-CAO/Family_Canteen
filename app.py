041d897866bd2192992093025b6d971e9ef068e0
Constance-CAO <connie88866@gmail.com>
2026-06-04 20:47:30 +1000

    在WSGI启动时自动建表

--- app.py
+++ app.py
@@ -614,7 +614,8 @@ def move_category():
     return redirect(url_for('admin_categories'))
 
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
